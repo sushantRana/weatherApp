316130dd31565105434610768434468657114f0f
sushant rana <sushant.rana31@gmail.com>
2018-07-12 18:14:27 +0530

    updated google map key.

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   APPID: 'a23c219b61541dabd9bd6b7bb9d7ee0f',
-  GoogleURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places',
+  GoogleURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBP2_Okzm1JzT2T1IJNOswN2W1OXnOb77E&v=3.exp&libraries=geometry,drawing,places',
   weatherAPIURL: 'http://api.openweathermap.org/data/2.5/forecast?'
 };
